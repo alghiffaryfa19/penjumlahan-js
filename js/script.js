ce777b5a394c968a43a1ac1f3270b383b7319c67
Fauzan Amir Al Ghiffary <56854433+alghiffaryfa19@users.noreply.github.com>
2020-11-29 11:56:32 +0700

    update js

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@ function calc() {
     var dua = document.getElementById("dua").value;
     if (satu == "" || dua == "") {
         alert("Inputan harus diisi");
+        document.getElementById("back").style.backgroundColor = "#F9672B";
         return false;
     } else {
         var hasil = parseInt(satu) + parseInt(dua);
