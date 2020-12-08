aebf040d3672145baf2b58e7d18f451b70404544
Ajeng Geby Syafitri <fauzanamiralghiffary@MacBook-Pro-Fauzan.local>
2020-12-08 10:09:07 +0700

    refactoring code js

--- js/script.js
+++ js/script.js
@@ -3,32 +3,41 @@ alert("Selamat Datang")
 function calc() {
     var satu = document.getElementById("satu").value;
     var dua = document.getElementById("dua").value;
+    var modal2 = document.getElementById("myModal2");
+    var close2 = document.getElementsByClassName("closee")[0];
+    var background = document.getElementById("back");
+
+    var modal1 = document.getElementById("myModal");
+    var close1 = document.getElementsByClassName("close")[0];
+
+    var total = document.getElementById("sum");
+
     if (satu == "" || dua == "") {
-        document.getElementById("myModal2").style.display = "block";
-        document.getElementsByClassName("closee")[0].onclick = function() {
-            document.getElementById("myModal2").style.display = "none";
+        modal2.style.display = "block";
+        close2.onclick = function() {
+            modal2.style.display = "none";
         }
 
         window.onclick = function(event) {
-            if (event.target == document.getElementById("myModal2")) {
-                document.getElementById("myModal2").style.display = "none";
+            if (event.target == modal2) {
+                modal2.style.display = "none";
             }
         }
-        document.getElementById("back").style.backgroundColor = "#F9672B";
+        background.style.backgroundColor = "#F9672B";
         return false;
     } else {
-        document.getElementById("myModal").style.display = "block";
-        document.getElementsByClassName("close")[0].onclick = function() {
-            document.getElementById("myModal").style.display = "none";
+        modal1.style.display = "block";
+        close1.onclick = function() {
+            modal1.style.display = "none";
         }
 
         window.onclick = function(event) {
-            if (event.target == document.getElementById("myModal")) {
-                document.getElementById("myModal").style.display = "none";
+            if (event.target == modal1) {
+                modal1.style.display = "none";
             }
         }
         var hasil = parseInt(satu) + parseInt(dua);
-        document.getElementById("sum").innerHTML = hasil;
-        document.getElementById("back").style.backgroundColor = "green";
+        total.innerHTML = hasil;
+        background.style.backgroundColor = "green";
     }
 }
